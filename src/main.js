3ffbd1601c04034196fe56fc88332395930a6a16
GudvinPRO <138712942+GudvinPRO@users.noreply.github.com>
2024-04-07 19:35:49 +0300

    1

--- src/main.js
+++ src/main.js
@@ -1,112 +1,64 @@
-import iziToast from 'izitoast';                                  // Бібліотека для повідомлень
-import 'izitoast/dist/css/iziToast.min.css';
+import { getImages } from './js/pixabay-api';
+import { renderImages } from './js/render-functions';
 
-import SimpleLightbox from 'simplelightbox';                      // Бібліотека для галереї
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const inputOfWords = document.querySelector('.inputOfWords');     // Інпут
-const buttonForInput = document.querySelector('.buttonForInput'); // Кнопка
-const userList = document.querySelector('.userList');             // Галерея
-const areaForLoader = document.querySelector('.areaForLoader');   // Лоадер
+const form = document.querySelector('.search-form');
+const list = document.querySelector('.list');
+const load = document.querySelector('.loading');
+form.addEventListener('submit', handleImages);
 
-let wordOfUser = '';
-let imagesLength = '';
+function handleImages(e) {
+  e.preventDefault();
+  load.classList.add('loader');
+  const input = e.target.elements.search.value.trim();
+  if (input === '') {
+    iziToast.error({
+      backgroundColor: 'lightred',
+      icon: false,
+      progressBar: false,
+      close: false,
+      position: 'topRight',
+      message: 'Please, fill the field!',
+    });
+    return;
+  }
 
-const lightbox = new SimpleLightbox('.gallery a', {   // Великі картинки 
-  captionDelay: 250,
-  captionsData: 'alt',
-});
-
-buttonForInput.addEventListener('click', event => {   // Надсилання запиту на сервер
-  loaderF();
-  event.preventDefault();
-  userList.innerHTML = '';
-  setTimeout(() => {
-    wordOfUser = inputOfWords.value.trim();
-    checkInputValidity();
-  }, 2000);
-});
-
-function checkInputValidity() {                   // Перевірка валідності запиту
-  fetchImages()
-    .then(images => {
-      if (wordOfUser === '') {
-        iziToast.show({
-          color: 'red',
-          message: `Sorry, the input field must be filled in to start the photo search.`,
-          position: 'topCenter',
+  getImages(input)
+    .then(value => {
+      if (value.hits && value.hits.length === 0) {
+        iziToast.error({
+          backgroundColor: 'red',
+          progressBar: false,
+          close: false,
+          position: 'topRight',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
         });
-      } else if (images.length === 0) {
-        iziToast.show({
-          color: 'red',
-          message: `Sorry, there are no images matching your search query. Please try again!`,
-          position: 'topCenter',
-        });
-      } else {
-        renderImg(images);
       }
+
+      const markup = renderImages(value.hits);
+      list.innerHTML = markup;
+      const lightbox = new SimpleLightbox('.gallery-link', {
+        captionsData: 'alt',
+      });
+      lightbox.refresh();
     })
-    .catch(error => console.log(error))
-    .finally(() => spanElementRem());
-}
-
-function renderImg(images) {                        // Рендар фото в браузері
-  imagesLength = images.length;
-
-  const markupImg = images                          
-    .map(image => {
-      return `<div class="blockForAllElements">
-          <li>
-          <a href=${image.largeImageURL} download="false">
-          <img src=${image.webformatURL} alt = "${image.tags}" class = "imgOfUser">
-          </a>
-          </li>
-          <div class = "divForDescription"> 
-          <ul class="blockOfInfo"> 
-            <li class="title">Likes</li>
-            <li class="info">${image.likes}</li>
-          </ul>
-          <ul class="block">
-            <li class="title">Views</li>
-            <li class="info">${image.views}</li>
-          </ul>
-          <ul class="block">
-            <li class="title">Comments</li>
-            <li class="info">${image.comments}</li>
-          </ul>
-          <ul class="block">
-            <li class="title">Downloads</li>
-            <li class="info">${image.downloads}</li>
-          </ul>
-          </div>
-        </div>`;
+    .catch(error => {
+      console.error('Error occurred while fetching images:', error);
+      iziToast.error({
+        backgroundColor: 'red',
+        icon: false,
+        progressBar: false,
+        close: false,
+        position: 'topRight',
+        message:
+          'Sorry, an error occurred while fetching images. Please try again!',
+      });
     })
-    .join('');
-  userList.insertAdjacentHTML('beforeend', markupImg);
-
-  lightbox.refresh();
-}
-
-function fetchImages() {                            // Запит на сервер для отримання фото
-  return fetch(
-    `https://pixabay.com/api/?key=42977219-0f6c9f9217f976d8651793c3a&q=${wordOfUser}&image_type=photo&per_page=15&orientation=horizontal&safesearch=true`
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(data => data.hits);
-}
-
-function loaderF() {                                  // Створюємо лоадер
-  const spanElement = document.createElement('span');
-  areaForLoader.appendChild(spanElement);
-  spanElement.classList.add('loader');
-}
-
-function spanElementRem() {                           // Видаляємо лоадер
-  const loaderF = document.querySelector('.loader');
-  loaderF.remove();
+    .finally(() => load.classList.remove('loader'));
+  e.target.reset();
 }
