ad9e4d2b48137faab485b177ffaf3221b45ed78e
GudvinPRO <138712942+GudvinPRO@users.noreply.github.com>
2024-04-07 16:35:32 +0000

    Deploying to gh-pages from @ GudvinPRO/goit-js-hw-11@3ffbd1601c04034196fe56fc88332395930a6a16 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,26 +1,29 @@
-import{S as u,i as c}from"./assets/vendor-8c59ed88.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const l of o.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&n(l)}).observe(document,{childList:!0,subtree:!0});function r(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function n(t){if(t.ep)return;t.ep=!0;const o=r(t);fetch(t.href,o)}})();const d=document.querySelector(".inputOfWords"),f=document.querySelector(".buttonForInput"),a=document.querySelector(".userList"),p=document.querySelector(".areaForLoader");let i="";const m=new u(".gallery a",{captionDelay:250,captionsData:"alt"});f.addEventListener("click",e=>{L(),e.preventDefault(),a.innerHTML="",setTimeout(()=>{i=d.value.trim(),h()},2e3)});function h(){g().then(e=>{i===""?c.show({color:"red",message:"Sorry, the input field must be filled in to start the photo search.",position:"topCenter"}):e.length===0?c.show({color:"red",message:"Sorry, there are no images matching your search query. Please try again!",position:"topCenter"}):y(e)}).catch(e=>console.log(e)).finally(()=>b())}function y(e){e.length;const s=e.map(r=>`<div class="blockForAllElements">
-          <li>
-          <a href=${r.largeImageURL} download="false">
-          <img src=${r.webformatURL} alt = "${r.tags}" class = "imgOfUser">
-          </a>
-          </li>
-          <div class = "divForDescription"> 
-          <ul class="blockOfInfo"> 
-            <li class="title">Likes</li>
-            <li class="info">${r.likes}</li>
-          </ul>
-          <ul class="block">
-            <li class="title">Views</li>
-            <li class="info">${r.views}</li>
-          </ul>
-          <ul class="block">
-            <li class="title">Comments</li>
-            <li class="info">${r.comments}</li>
-          </ul>
-          <ul class="block">
-            <li class="title">Downloads</li>
-            <li class="info">${r.downloads}</li>
-          </ul>
-          </div>
-        </div>`).join("");a.insertAdjacentHTML("beforeend",s),m.refresh()}function g(){return fetch(`https://pixabay.com/api/?key=42977219-0f6c9f9217f976d8651793c3a&q=${i}&image_type=photo&per_page=15&orientation=horizontal&safesearch=true`).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()}).then(e=>e.hits)}function L(){const e=document.createElement("span");p.appendChild(e),e.classList.add("loader")}function b(){document.querySelector(".loader").remove()}
+import{i as a,S as f}from"./assets/vendor-8c59ed88.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();function m(s){const o="https://pixabay.com/api/",t=new URLSearchParams({key:"43096972-1afb589163252f275da428b1d",q:s,image_type:"photo",orientation:"horizontal",safesearch:"true"}),i=`${o}?${t}`;return fetch(i).then(e=>e.json()).catch(e=>{throw console.error("Error occurred while fetching images:",e),e})}function u(s){return s.map(({webformatURL:o,largeImageURL:t,tags:i,likes:e,views:r,comments:n,downloads:c})=>`<li class="list-item">
+     <a class="gallery-link" href ="${t}">
+     <img src="${o}" alt="${i}" class="gallery-image"
+     </a>
+     
+      <ul class="information-list">
+        <li class="item-information-container">
+          <h2 class="main-info">Likes </h2>
+            <p class="info">${e}</p>
+          
+        </li>
+        <li class="item-information-container">
+          <h2 class="main-info"> Views</h2>
+            <p class="info">${r}</p>
+          
+        </li>
+        <li class="item-information-container">
+          <h2 class="main-info">Comments </h2>
+            <p class="info">${n}</p>
+          
+        </li>
+        <li class="item-information-container">
+          <h2 class="main-info">Downloads </h2>
+            <p class="info">${c}</p>
+        </li>
+      </ul>
+   
+    </li>`).join("")}const h=document.querySelector(".search-form"),d=document.querySelector(".list"),l=document.querySelector(".loading");h.addEventListener("submit",g);function g(s){s.preventDefault(),l.classList.add("loader");const o=s.target.elements.search.value.trim();if(o===""){a.error({backgroundColor:"lightred",icon:!1,progressBar:!1,close:!1,position:"topRight",message:"Please, fill the field!"});return}m(o).then(t=>{t.hits&&t.hits.length===0&&a.error({backgroundColor:"red",progressBar:!1,close:!1,position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"});const i=u(t.hits);d.innerHTML=i,new f(".gallery-link",{captionsData:"alt"}).refresh()}).catch(t=>{console.error("Error occurred while fetching images:",t),a.error({backgroundColor:"red",icon:!1,progressBar:!1,close:!1,position:"topRight",message:"Sorry, an error occurred while fetching images. Please try again!"})}).finally(()=>l.classList.remove("loader")),s.target.reset()}
 //# sourceMappingURL=commonHelpers.js.map
