7d1a6b72851b05e20cefce8998dc9dc6c6f0f24a
GudvinPRO <138712942+GudvinPRO@users.noreply.github.com>
2024-04-22 22:02:21 +0300

    1

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -6,7 +6,9 @@ const lightbox = new SimpleLightbox('.gallery-link', {
 });
 
 export function renderImages(imagesArr) {
-  return imagesArr
+  const galleryContainer = document.querySelector('.gallery');
+
+  const imagesHTML = imagesArr
     .map(
       ({
         webformatURL,
@@ -18,31 +20,33 @@ export function renderImages(imagesArr) {
         downloads,
       }) => {
         return `<li class="list-item">
-     <a class="gallery-link" href ="${largeImageURL}">
-     <img src="${webformatURL}" alt="${tags}" class="gallery-image"
-     </a>
-      <ul class="information-list">
-        <li class="item-information-container">
-          <h2 class="main-info">Likes </h2>
-            <p class="info">${likes}</p>
-        </li>
-        <li class="item-information-container">
-          <h2 class="main-info"> Views</h2>
-            <p class="info">${views}</p>
-        </li>
-        <li class="item-information-container">
-          <h2 class="main-info">Comments </h2>
-            <p class="info">${comments}</p>
-        </li>
-        <li class="item-information-container">
-          <h2 class="main-info">Downloads </h2>
-            <p class="info">${downloads}</p>
-        </li>
-      </ul>
-    </li>`;
+          <a class="gallery-link" href="${largeImageURL}">
+            <img src="${webformatURL}" alt="${tags}" class="gallery-image">
+          </a>
+          <ul class="information-list">
+            <li class="item-information-container">
+              <h2 class="main-info">Likes </h2>
+              <p class="info">${likes}</p>
+            </li>
+            <li class="item-information-container">
+              <h2 class="main-info"> Views</h2>
+              <p class="info">${views}</p>
+            </li>
+            <li class="item-information-container">
+              <h2 class="main-info">Comments </h2>
+              <p class="info">${comments}</p>
+            </li>
+            <li class="item-information-container">
+              <h2 class="main-info">Downloads </h2>
+              <p class="info">${downloads}</p>
+            </li>
+          </ul>
+        </li>`;
       }
     )
     .join('');
-    galleryContainer.insertAdjacentHTML('beforeend', imagesHTML);
-    lightbox.refresh();
+
+  galleryContainer.insertAdjacentHTML('beforeend', imagesHTML);
+
+  lightbox.refresh();
 }
